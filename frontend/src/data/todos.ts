9c19edaed083171f2e0b1469f76256f464899343
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-11-24 23:12:52 +0100

    todo frontpage wip

---
+++ frontend/src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Like this video ❤️",
+        completed:false
+    },
+    {
+        id: 2,
+        title: "Subscribe to coding in flow 💡",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Leave a nice comment 😊",
+        completed: false
+    }
+]
