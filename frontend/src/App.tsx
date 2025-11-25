7de6de272a2a3a96931d8fadc91fe3616c4abe71
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-11-25 23:45:25 +0100

    frontend wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
-import TodoItem from "./components/TodoItems"
+import { useEffect, useState } from "react"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/Todolist";
+import TodoSummary from "./components/TodoSummary";
+import type { Todo } from "./types/todos";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return savedTodos.length > 0 ? savedTodos : dummyData
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   function setTodoCompleted(id: number, completed: boolean) {
     setTodos((prevTodos) => 
@@ -15,31 +24,39 @@ function App() {
   function addTodo(title: string) {
     setTodos((prevTodos) => [
       {
-        id: prevTodos.length + 1,
+        id: Date.now(),
         title,
         completed: false
       }, 
       ...prevTodos,
     ])
   }
+
+  function deleteTodo(id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  function deleteAllCompletedTodos() {
+    setTodos(prevTodos => prevTodos.filter(todo => !todo.completed))
+  }
+
   return (
-    <main className="py-10 h-screen space-y-2">
+    <main className="py-10 h-screen space-y-2 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">
         Your Todos
       </h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo}/> 
-        <div className="space-y-2">
-          {
-            todos.map((todo) => (
-              <TodoItem 
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-              />
-            ))}
-        </div>
+        <TodoList 
+          todos={todos} 
+          onCompletedChange={setTodoCompleted} 
+          onDelete={deleteTodo}
+        />
       </div>
+      <TodoSummary
+      todos={todos}
+      deleteAllCompleted={deleteAllCompletedTodos}
+      />
     </main>
   )
 }
